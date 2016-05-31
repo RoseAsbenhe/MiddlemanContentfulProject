774a5cf6169816a61dcc912efb48eef74ec2585e
RoseAsbenhe <boehner.vanessa@gmail.com>
2016-05-31 17:57:14 +0200

    initial commit

---
+++ config.rb
@@ -0,0 +1,49 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+
+activate :contentful do |f|
+  f.access_token  = '267d41058d049409924c6b4315326df31854c91240def4a73bf195f95e6e5f77'
+  f.space         = { site: 'b3o0q75q67ev' }
+end
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
